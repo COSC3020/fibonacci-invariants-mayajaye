62950f6a2a710443c89b361df4454cf67c20d8ba
Maya Conway <mconway4@uwyo.edu>
2025-03-14 14:21:57 -0600

    fib invariants with the normal amount of arrays

--- code.js
+++ code.js
@@ -2,13 +2,14 @@ function fib(n, fib_solns = {}) {
     if (n == 0) return [0];
     if (n == 1) return [0, 1]; //base cases <-^
 
-    if (!fib_solns[n]) { //if n is not in the array
-        const leftNum1 = fib(n - 1, fib_solns); //recursively compute n - 1
-        const leftNum2 = fib(n - 2, fib_solns); //and n - 2
-
-        const fib_n = leftNum1[leftNum1.length - 1] + leftNum2[leftNum2.length - 1]; //compute fib of current n with the
-        fib_solns[n] = leftNum1.concat([fib_n]);                                     //last values of the arrays then                                                                                     
-    }                                                                                //add fib(n) to the end of the array
+    if (!fib_solns[n]) { //if n is not in the computed
+        if (!fib_solns[n - 1]) { //and if n - 1 is not computed
+            fib_solns[n - 1] = fib(n - 1, fib_solns); //compute fib of n - 1 and store it
+        }
+        const prevFibs = fib_solns[n-1];                                             //compute fib of current n using
+        const fib_n = prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]; //the values in the array then
+        fib_solns[n] = prevFibs.concat([fib_n]);                                     //add fib(n) to the end of the array                                                  
+    }
     return fib_solns[n];
 }
 
